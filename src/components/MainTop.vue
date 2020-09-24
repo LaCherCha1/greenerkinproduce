3e94f47fbb11b7fd1af5537bb588a7eb0a0c3058
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-09-24 01:04:47 -0400

    added background using props

--- src/components/MainTop.vue
+++ src/components/MainTop.vue
@@ -1,13 +1,15 @@
 <template>
-	<div class="main-top" id="home">
-        <slot></slot>
+	<div class="main-top" :style="image" id="home">
+		<slot></slot>
 		<!-- banner -->
 		<div class="main-banner text-center">
 			<div class="container">
 				<div class="style-banner">
-					<h4 class="mb-2">Beyond just planting a seed… we contribute to a better future</h4>
+					<h4 class="mb-2">
+						Beyond just planting a seed… we contribute to a better future
+					</h4>
 					<h5>FOR OUR PEOPLE AND THE ENVIRONMENT</h5>
-				</div>				
+				</div>
 			</div>
 		</div>
 	</div>
@@ -16,7 +18,16 @@
 
 <script>
 export default {
-    name: "MainTop"
-   
+	name: "MainTop",
+	data() {
+		return {
+			image: {
+				background: `url(${require("@/assets/images/b3.jpg")}) no-repeat bottom`,
+				backgroundSize: "cover",
+			},
+		};
+	},
 };
 </script>
+
+<style scoped></style>
